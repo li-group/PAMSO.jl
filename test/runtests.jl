00986c02f669343743ef9bd273b3a5f3f03617a4
asharamanujam <103427149+asharamanujam@users.noreply.github.com>
2024-06-27 13:06:17 -0400

    Add files via upload

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,20 @@
 using PAMSO
 using Test
-
+using Random
+Random.seed!(1)
 @testset "PAMSO.jl" begin
     # Write your tests here.
-    @test true
+    root = pwd()
+    cd(joinpath(root,"examples","Gen_expanison"))
+    include("high_level_model.jl")
+	include("low_level_model.jl")
+	PAMSO_toy = PAMSO.PAMSO_block(gen_highlevel, gen_lowlevel, "MADS", 2, ["R","R"],[0.0,0.0],[10.0,100.0],[1.0,1.0],300)
+	PAMSO.run(PAMSO_toy)
+    @test isapprox(PAMSO_toy.MBBF(PAMSO_toy.Param_best), 167415.4166,rtol = 0.01)
+    PAMSO_toy = PAMSO.PAMSO_block(gen_highlevel, gen_lowlevel, "Bayesopt", 2, ["R","R"],[0.0,0.0],[10.0,100.0],[1.0,1.0],300)
+	PAMSO.run(PAMSO_toy)
+    @test isapprox(PAMSO_toy.MBBF(PAMSO_toy.Param_best), 167415.4166,rtol = 0.01)
+    PAMSO_toy = PAMSO.PAMSO_block(gen_highlevel, gen_lowlevel, "PSO", 2, ["R","R"],[0.0,0.0],[10.0,100.0],[1.0,1.0],300)
+	PAMSO.run(PAMSO_toy)
+    @test isapprox(PAMSO_toy.MBBF(PAMSO_toy.Param_best), 167415.4166,rtol = 0.01)
 end
