15a26c90a4e8d037fd9004e854894ec4b60eff7e
asharamanujam <103427149+asharamanujam@users.noreply.github.com>
2024-06-28 14:20:24 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,16 +4,14 @@ using Random
 Random.seed!(1)
 @testset "PAMSO.jl" begin
     # Write your tests here.
+    PAMSO_toy = PAMSO.gen_problem("Generator expansion")
     optimize!(PAMSO_toy.full_space_model)
     obj_actual = objective_value(PAMSO_toy.full_space_model)
-    PAMSO_toy = PAMSO.gen_problem("Generator expansion")
     PAMSO.run(PAMSO_toy,"MADS",300)
     @test isapprox(PAMSO_toy.MBBF(PAMSO_toy.Param_best), obj_actual,rtol = 0.01)
-    obj_actual = objective_value(PAMSO_toy.full_space_model)
     PAMSO_toy = PAMSO.gen_problem("Generator expansion")
     PAMSO.run(PAMSO_toy,"Bayesopt",300)
     @test isapprox(PAMSO_toy.MBBF(PAMSO_toy.Param_best), obj_actual,rtol = 0.01)
-    obj_actual = objective_value(PAMSO_toy.full_space_model)
     PAMSO_toy = PAMSO.gen_problem("Generator expansion")
     PAMSO.run(PAMSO_toy,"PSO",300)
     @test isapprox(PAMSO_toy.MBBF(PAMSO_toy.Param_best), obj_actual,rtol = 0.01)
