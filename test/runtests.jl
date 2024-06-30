c98df2604b4377f4394a1c9d275bf4a8ad55fa6a
asharamanujam <103427149+asharamanujam@users.noreply.github.com>
2024-06-30 10:25:31 -0400

    Add files via upload

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,13 @@
 using PAMSO
 using Test
 using Random
+using JuMP
 Random.seed!(1)
 
 @testset "PAMSO.jl" begin
     # Write your tests here.
     cd(dirname(pathof(PAMSO)))
+    println(pwd())
     PAMSO_toy = PAMSO.gen_problem("Generator expansion")
     optimize!(PAMSO_toy.full_space_model)
     obj_actual = objective_value(PAMSO_toy.full_space_model)
