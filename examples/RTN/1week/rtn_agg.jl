8ec433940db858191cb43af7110b5621815213c4
asharamanujam <103427149+asharamanujam@users.noreply.github.com>
2024-07-03 10:04:05 -0400

    Update rtn_agg.jl

--- examples/RTN/1week/rtn_agg.jl
+++ examples/RTN/1week/rtn_agg.jl
@@ -216,7 +216,7 @@ end
     #@constraint(m,sl.==0)
 
     obj = (
-        sum(N[i, t] for i in I for t in T1)*param_n +
+        sum(N[i, t]*N_cost[i] for i in I for t in T1)*param_n +
         sum(pi[r, t] * R_cost[r] for r in R for t in T1) +
         param_b1 * sum(sl[r, t] * R_cost[r] for r in Rp for t in Td) +
         sum(Vmax_pow[r] * R_cost[r] * param_dof* weeks  for r in Feed_vessels) +
