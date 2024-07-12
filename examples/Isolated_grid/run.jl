2b02e551d92a000c590d9512295a95935d18ff48
asharamanujam <103427149+asharamanujam@users.noreply.github.com>
2024-07-12 11:27:14 -0400

    Delete examples/Isolated_grid/run.jl

--- examples/Isolated_grid/run.jl
+++
@@ -1,7 +0,0 @@
-root = pwd()
-include(joinpath(root,"high_level_model.jl"))
-include(joinpath(root,"low_level_model.jl"))
-using PAMSO
-
-PAMSO_iso = PAMSO.PAMSO_block(gen_highlevel, gen_lowlevel, "MADS", 2, ["I","R"],[0.0,0.0],[1000,1.0],[1.0,1.0],2)
-PAMSO.run(PAMSO_iso)
