5dcff8ed902af63c568386e6a082c3ea8aeaecb9
asharamanujam <103427149+asharamanujam@users.noreply.github.com>
2024-07-12 11:26:42 -0400

    Delete examples/Connected_grid/200_location/run.jl

--- examples/Connected_grid/200_location/run.jl
+++
@@ -1,7 +0,0 @@
-root = pwd()
-include(joinpath(root,"high_level_model.jl"))
-include(joinpath(root,"low_level_model.jl"))
-using PAMSO
-
-PAMSO_200loc = PAMSO.PAMSO_block(gen_highlevel, gen_lowlevel, "MADS", 2, ["R","R"],[0.0,0.0],[1.0,1.0],[1.0,1.0],2)
-PAMSO.run(PAMSO_200loc)
